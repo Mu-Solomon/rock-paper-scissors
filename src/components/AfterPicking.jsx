2755ada5c7b3774ccd87947b9a0f97f80ee067b3
Mu-Solomon <musolomoni@gmail.com>
2026-01-25 05:11:42 +0300

    Responsive after picking done

--- src/components/AfterPicking.jsx
+++ src/components/AfterPicking.jsx
@@ -2,35 +2,37 @@ export default function AfterPicking() {
   return (
     <>
       {" "}
-      <div className="flex  w-full  mt-24 mb-20  gap-5  bg-no-repeat  bg-center flex-wrap justify-between items-center  ">
+      <div className="flex  w-full  mt-24 mb-20 sm:mt-40  gap-5 sm:gap-18 bg-no-repeat  bg-center flex-wrap justify-between sm:justify-center items-center  ">
         <div className="h-50 flex flex-col items-center justify-center ">
-          <div className="h-36 flex flex-col ">
-            <div className="w-32 h-32 bg-[#dc2e4eff] flex justify-center items-center rounded-full mx-auto">
-              <div className="bg-white rounded-full w-22 h-22 p-6 mx-auto inset-shadow-sm/50 inset-shadow-black">
+          <div className="h-36 sm:h-90 flex flex-col sm:order-2">
+            <div className="w-32 h-32 sm:h-68 sm:w-68 bg-[#dc2e4eff] flex justify-center items-center rounded-full mx-auto">
+              <div className="bg-white rounded-full w-22 h-22 sm:h-50 sm:w-50 p-6 mx-auto inset-shadow-sm/50 inset-shadow-black">
                 <img
                   src="/images/icon-rock.svg"
-                  className="mx-auto"
+                  className="mx-auto sm:w-28 sm:mt-6"
                   alt="Rock Icon"
                 />
               </div>
             </div>{" "}
           </div>
-          <p className="text-center text-white font-semibold">YOU PICKED</p>
+          <p className="text-center text-white font-semibold sm:order-1 sm:my-16 sm:text-2xl">
+            YOU PICKED
+          </p>
         </div>
 
         <div className="h-50 flex flex-col items-center justify-center ">
-          <div className="h-36 flex flex-col ">
-            <div className="w-32 h-32 bg-[#ec9e0eff] flex justify-center items-center rounded-full mx-auto">
-              <div className="bg-white rounded-full w-22 h-22 p-6 mx-auto inset-shadow-sm/50 inset-shadow-black">
+          <div className="h-36 sm:h-90 flex flex-col sm:order-2">
+            <div className="w-32 h-32 sm:h-68 sm:w-68 bg-[#ec9e0eff] flex justify-center items-center rounded-full mx-auto">
+              <div className="bg-white rounded-full w-22 h-22 sm:h-50 sm:w-50 p-6 mx-auto inset-shadow-sm/50 inset-shadow-black">
                 <img
                   src="/images/icon-scissors.svg"
-                  className="mx-auto"
+                  className="mx-auto sm:w-28"
                   alt="Scissors Icon"
                 />
               </div>
             </div>{" "}
           </div>
-          <p className="text-center text-white font-semibold">
+          <p className="text-center text-white font-semibold order-1 sm:my-16 sm:text-2xl">
             THE HOUSE PICKED
           </p>
         </div>
